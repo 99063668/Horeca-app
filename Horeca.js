a8407295315e7fbdd565f0dce682ed26fde02ae5
Dewi van der Velden <70890464+99063668@users.noreply.github.com>
2020-10-26 12:11:37 +0100

    Add files via upload

--- Horeca.js
+++ Horeca.js
@@ -1,4 +1,37 @@
-var antwoord;
+function KeuzeDrankje(){
+    document.write('<br>Drankje:'+ prompt('Welke bestelling wilt u toevoegen? Keuze uit fris, wijn of bier'));
+}
+KeuzeDrankje();
+
+
+function Fris(){
+    document.write('<br>Aantal: ' + prompt('Hoeveel fris wilt u toevoegen aan uw bestelling?'));
+}
+Fris();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*var antwoord;
 
 antwoord=prompt('Welke bestelling wilt u toevoegen? Keuze uit fris, wijn of bier');
 
@@ -15,4 +48,4 @@ if(antwoord=="fris"){
         if(antwoord=="bier"){
              document.write("Keuze: bier");
              document.write('<br>Aantal bier: ' + prompt('Hoeveel bier wilt u toevoegen aan uw bestelling?'));
-        }
+        }*/
