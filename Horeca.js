6a226b71d8c1ea16f1989705913529e890ac42be
Dewi van der Velden <70890464+99063668@users.noreply.github.com>
2020-11-03 13:33:23 +0100

    Add files via upload

--- Horeca.js
+++ Horeca.js
@@ -4,11 +4,15 @@ var invoer = null;
 const PriceFris = 1.25;
 const PriceWijn = 1.50;
 const PriceBier = 1.50;
+const PriceBitter6 = 2;
+const PriceBitter18 = 2.25;
 
 //Aantal 1 product
 var AantalFris = 0;
 var AantalWijn = 0;
 var AantalBier = 0;
+var AantalBitter6 = 0;
+var AantalBitter18 = 0;
 
 
 //Invoer
@@ -30,20 +34,38 @@ while(invoer != "stop"){
         invoer = parseInt(prompt("Hoeveel bier wilt u bestellen?"));
         AantalBier = AantalBier + invoer;
     }
+    if (invoer == "bitterballen")
+    {
+        invoer = parseInt(prompt("Wilt u een schaal van 6 of 18 bitterballen bestellen?"));
+    }
+    if (invoer == "6")
+    {
+        invoer = parseInt(prompt("Hoeveel schaalen van 6 bitterballen wilt u bestellen?"));
+        AantalBitter6 = AantalBitter6 + invoer;
+    }
+    if (invoer == "18")
+    {
+        invoer = parseInt(prompt("Hoeveel schaalen van 18 bitterballen wilt u bestellen?"));
+        AantalBitter18 = AantalBitter18 + invoer;
+    }
 }
 
 //Totaal bedrag 1 product
 var BetaaldFris = AantalFris * PriceFris;
 var BetaaldWijn = AantalWijn * PriceWijn;
 var BetaaldBier = AantalBier * PriceBier;
+var BetaaldBitter6 = AantalBitter6 * PriceBitter6;
+var BetaaldBitter18 = AantalBitter18 * PriceBitter18;
 
 //Prijs drankjes
 document.write("<br>" + AantalFris +  " Fris: €" + BetaaldFris);
 document.write("<br>" + AantalWijn +  " Wijn: €" + BetaaldWijn);
 document.write("<br>" + AantalBier + " Bier: €" + BetaaldBier);
+document.write("<br>" + AantalBitter6 + " Schaalen van 6 bitterballen: €" + BetaaldBitter6);
+document.write("<br>" + AantalBitter18 + " Schaalen van 18 bitterballen: €" + BetaaldBitter18);
 
 //Berekening totaalbedrag
-var Totaalbedrag = BetaaldFris + BetaaldWijn + BetaaldBier;
+var Totaalbedrag = BetaaldFris + BetaaldWijn + BetaaldBier + BetaaldBitter6 + BetaaldBitter18;
 
 document.write('<br> Totaalbedrag: €' + Totaalbedrag);
 
